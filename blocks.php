2cb844d14517341a5ff521f20e95c10e6dddb976
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:14:05 -0700

    Corrects underpin autoloader

--- blocks.php
+++ blocks.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Blocks.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Block.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Block_Instance.php' );
-		Underpin\underpin()->get( $file )->loaders()->add( 'blocks', [
+		Underpin\underpin()->get( $file, $class )->loaders()->add( 'blocks', [
 			'registry' => 'Underpin_Blocks\Loaders\Blocks',
 		] );
-} );
+}, 10, 2 );
